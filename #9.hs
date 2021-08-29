793899eff2f5d016b833b09b08c9faa0be37a85f
Luís Fernando de Sá Amorim <40233089+MoonCancerBR@users.noreply.github.com>
2021-08-29 02:27:40 -0300

    Update #9.hs

--- #9.hs
+++ #9.hs
@@ -1,54 +1,65 @@
+
 import qualified Data.Map as Map
-import Data.Either
-import Data.Map
-import Data.Maybe
+import Data.Either ( fromLeft, fromRight )
+import Data.Map ()
+import Data.Maybe ( isJust )
 data Estado = Alugado | Livre deriving (Show, Eq)
 type Codigo = String
 type Armario = (Estado, Codigo)
 type Armarios = Map.Map Int (Estado, Codigo)
 
-restaurado (Just z) = z
+-- consulta : dado o número de um armário, retorna se ele está alugado ou não
+-- retorna Nothing se o armário não existir
 
-consulta indice armario =  atual
-    where
-        atual = Map.lookup indice armario
+consulta :: Int -> Armarios -> Maybe Estado
+consulta indice armarios = 
+  case Map.lookup indice armarios of
+  Nothing -> Nothing
+  Just (estado, codigo) -> if estado /= Alugado
+  then Just Livre
+  else Just Alugado
 
-pegarArmario indice armarios
-    | isNothing atual = Data.Either.Left "Armario nao existe"
-    | otherwise = Data.Either.Right $ restaurado $ atual
-    where
-            atual =  consulta indice armarios
+-- dado o número do armário, retorna Right armário ou Left "Armario nao existe"
 
-pegarCodigo indice armarios
-    | isLeft eitherAtual = Data.Either.Left "Armario nao existe"
-    | otherwise = rep
-     where
-        eitherAtual = pegarArmario indice armarios
-        atual =  Data.Either.fromRight (Alugado,"") eitherAtual
-        rep
-            | fst atual == Livre = Data.Either.Right (snd atual)
-            | otherwise = Data.Either.Left "ja esta ocupado"
+pegarArmario :: Int -> Armarios -> Either String Armario
+pegarArmario indice armarios = case Map.lookup indice armarios of  
+  Nothing -> Left $ "Armario " ++ show indice ++ " nao existe"
+  Just (estado, codigo) -> if Data.Maybe.isJust (consulta indice armarios) 
+  then Right (estado, codigo)  
+  else Left $ "Armario " ++ show indice ++ " nao existe"
 
-aluguel indice armarios
-    |isRight eitherAtual = Data.Either.Right g
-    | otherwise =  Data.Either.Left "Armario ja esta ocupado"
-    where
-        eitherAtual = pegarCodigo indice armarios
-        atual = Data.Either.fromRight "" eitherAtual
+-- consultar: dado o número de um armário retorna "Right código"
+-- Se não existir, retorna "Left Armario não existe", se já estiver alugado retorna "Left ja esta ocupado"
+-- Utilize a função pegarArmario para obter o armário e verificar se ele existe
+-- Não reescreva a mesma mensagem de erro, utilize a invocação do pegarArmário
 
-        g = Map.map (\b -> if snd b == atual then (Alugado,snd b) else b) armarios
+pegarCodigo :: Int -> Armarios -> Either String Codigo
+pegarCodigo indice armarios = case pegarArmario indice armarios of  
+  Left string -> Left string
+  Right (estado, codigo) -> if estado == Livre
+  then Right codigo
+  else Left $ "Armario " ++ show indice ++ " ja esta ocupado"
 
-devolucao indice codigo armarios
-    |isLeft eitherAtual = Data.Either.Left "Armario nao existe"
-    |otherwise = auxiliar
-    where
-        eitherAtual = pegarArmario indice armarios
-        atual = Data.Either.fromRight (Livre,"") eitherAtual
-        g = Map.map (\b -> if snd b == snd atual then (Livre,snd b) else b) armarios
-        auxiliar
-            | codigo /= snd atual = Data.Either.Left "Codigo incorreto"
-            | fst atual == Livre = Data.Either.Left "nao esta alugado"
-            | otherwise = Data.Either.Right g
+-- aluguel - se o armário existir e estiver desocupado, o estado do armário no mapa é alterado para alugado
+-- reaproveite a função pegarCodigo para obter a chave atual e os erros de "não existe" e "está ocupado"
+
+aluguel :: Int -> Armarios -> Either String Armarios
+aluguel indice armarios = case pegarCodigo indice armarios of
+  Left string -> Left string
+  Right codigoArm -> Right (Map.insert indice (Alugado, codigoArm) armarios)
+
+-- para devolver um armário é necessário que ele exista, que esteja alugado e que o código esteja correto
+-- utilize a função pegar armário para verificar a existência.
+-- Se o armário não estiver alugado ou se o código estiver incorreto avise utilizando o Left.
+
+devolucao :: Int -> Codigo -> Armarios -> Either String Armarios
+devolucao indice codigo armarios = case pegarArmario indice armarios of
+  Left string -> Left "oasjdoiasijdasd"
+  Right (estadoArm, codigoArm) -> if estadoArm == Livre
+  then Left "Armario nao esta alugado"
+  else if codigoArm /= codigo
+  then Left "Codigo incorreto"
+  else Right (Map.insert indice (Livre, codigo) armarios)                      
 
 lockers :: Armarios
 lockers = Map.fromList
@@ -59,3 +70,42 @@ lockers = Map.fromList
     ,(109,(Alugado,"893JJ"))
     ,(110,(Alugado,"99292"))
     ]
+
+main :: IO ()
+main = do
+    let armarios = lockers
+    print $ armarios
+    print $ consulta 100 armarios
+    print $ consulta 101 armarios
+    print $ consulta 200 armarios
+    print $ pegarCodigo 100 armarios
+    print $ pegarCodigo 101 armarios
+    print $ pegarCodigo 200 armarios
+    print $ aluguel 100 armarios
+    print $ consulta 101 armarios
+    let armarios1 = fromRight armarios $ aluguel 101 armarios
+    print $ consulta 101 armarios1
+    print $ fromLeft "" $ devolucao 103 "IQSA9" armarios1
+    print $ fromLeft "" $ devolucao 101 "ZD39I" armarios1
+    let armarios2 = fromRight armarios1 $ devolucao 110 "99292" armarios1
+    print $ consulta 110 armarios2
+    print $ armarios2
+    
+{--
+
+fromList [(100,(Alugado,"ZD39I")),(101,(Livre,"JAH3I")),(103,(Livre,"IQSA9")),(105,(Livre,"QOTSA")),(109,(Alugado,"893JJ")),(110,(Alugado,"99292"))]
+Just Alugado
+Just Livre
+Nothing
+Left "Armario 100 ja esta ocupado"
+Right "JAH3I"
+Left "Armario 200 nao existe"
+Left "Armario 100 ja esta ocupado"
+Just Livre
+Just Alugado
+"Armario nao esta alugado"
+"Codigo incorreto"
+Just Livre
+fromList [(100,(Alugado,"ZD39I")),(101,(Alugado,"JAH3I")),(103,(Livre,"IQSA9")),(105,(Livre,"QOTSA")),(109,(Alugado,"893JJ")),(110,(Livre,"99292"))]
+
+--}
